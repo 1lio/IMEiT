aaeb049aebcaca3d723dd9d4a31615aeb3447f57
SukhovViacheslav <svoefremov@yandex.ru>
2019-05-25 10:05:56 +0300

    мелкие исправления

--- app/src/main/java/ru/vyaacheslav/suhov/imeit/view/ftagments/other/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/ru/vyaacheslav/suhov/imeit/view/ftagments/other/BottomNavigationDrawerFragment.kt
@@ -5,37 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.fr_bottomsheet.*
 import ru.vyaacheslav.suhov.imeit.R
 import ru.vyaacheslav.suhov.imeit.view.ftagments.calls.CallFragment
 import ru.vyaacheslav.suhov.imeit.view.ftagments.maps.MapsPagerFragment
-import ru.vyaacheslav.suhov.imeit.view.ftagments.schedule.SchedulePagerFragment
-import ru.vyaacheslav.suhov.imeit.viewmodel.MainViewModel
 
-// TODO: ОБНОВЛЕНИЕ ФРАГМЕНТОВ В КОНТЕЙНЕРЕ!!!
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
-
-    private lateinit var model: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fr_bottomsheet, container, false)
-        model = ViewModelProviders.of(activity!!)[MainViewModel::class.java]
+        return v
+    }
 
-        val navigation: NavigationView = v.findViewById(R.id.navigation_view)
-        navigation.setOnClickListener {
-            when (it!!.id) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+
+            when (menuItem.itemId) {
                 R.id.bells_info -> pushFragment(CallFragment())
                 R.id.location -> pushFragment(MapsPagerFragment())
-                R.id.schedule -> {
-                    if (model.isSelectedGroup()) pushFragment(SchedulePagerFragment())
-                    else pushFragment(EmptyGroupFragment())
-                }
+                /*   R.id.schedule -> {
+                       if (model.isSelectedGroup()) pushFragment(SchedulePagerFragment())
+                       else pushFragment(EmptyGroupFragment())
+                   }*/
+
             }
+            true
+
+            //TODO: ОТКРЕПИТЬ ДИАЛОГ ФРАГМЕНТ ПОСЛЕ НАЖАТИЯ
         }
-        return v
+
+
     }
 
     private fun pushFragment(fragment: Fragment) {
