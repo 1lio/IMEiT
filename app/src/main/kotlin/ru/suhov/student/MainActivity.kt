12eeeb83ab38930b8aebd9c98ad585cd63ba5270
SukhovViacheslav <svo_efremov@mail.ru>
2020-06-15 14:18:46 +0300

    minor

--- app/src/main/kotlin/ru/suhov/student/MainActivity.kt
+++ app/src/main/kotlin/ru/suhov/student/MainActivity.kt
@@ -1,11 +1,7 @@
 package ru.suhov.student
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import ru.student.assistant.auth.ui.AuthFragment
 
 class MainActivity : AppCompatActivity() {
@@ -17,30 +13,9 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .addToBackStack(null)
                 .add(R.id.container, AuthFragment())
-                .commitNow()
+                .commit()
         }
-
     }
 
-
-    private fun checkPermissions(permission: String, requestCode: Int): Boolean {
-
-        return if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, REQUEST_PERMISSIONS, requestCode)
-            false
-        } else true
-    }
-
-    private companion object {
-
-        const val PERMISSION_REQ_ID = 22
-
-        val REQUEST_PERMISSIONS = arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-        )
-    }
 }
