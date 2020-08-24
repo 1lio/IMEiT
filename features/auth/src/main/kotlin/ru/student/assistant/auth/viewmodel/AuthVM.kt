0adf0f00fcea33541181302ad3964cefb03af4d8
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-24 16:32:41 +0300

    Minor

--- features/auth/src/main/kotlin/ru/student/assistant/auth/viewmodel/AuthVM.kt
+++ features/auth/src/main/kotlin/ru/student/assistant/auth/viewmodel/AuthVM.kt
@@ -1,15 +1,25 @@
 package ru.student.assistant.auth.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 
 class AuthVM : ViewModel() {
 
     private val state = MutableLiveData<Byte>()
+    private var isFirstRun = true
 
+    init {
 
+    }
 
-    private companion object {
+    fun observeState(owner: LifecycleOwner, observer: Observer<Byte>){
+        state.observe(owner, observer)
+    }
+
+    fun setState(state: Byte){
+        this@AuthVM.state.value = state
+    }
+
+    companion object {
         const val SIGN_IN: Byte = 0
         const val SIGN_UP: Byte = 1
         const val RESTORE: Byte = 2
