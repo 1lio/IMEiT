9e9932c96d99c16357db418820706983b0920b66
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-24 15:34:15 +0300

    Add modules ext file

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,17 @@
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
+object Modules {
+
+    const val CORE = "core"
+    const val REPO = "repo"
+    const val NETWORK = "network"
+
+    const val AUTH = "auth"
+    const val ACCOUNT = "account"
+    const val CALLS = "calls"
+    const val SCHEDULE = "schedule"
+    const val MAPS = "maps"
+
+
+}
+
