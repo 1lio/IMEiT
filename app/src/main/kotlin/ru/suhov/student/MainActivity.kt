62ef297d9411c1b16b2c6656cb62a94d15262dd8
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-24 15:34:31 +0300

    Update contract

--- app/src/main/kotlin/ru/suhov/student/MainActivity.kt
+++ app/src/main/kotlin/ru/suhov/student/MainActivity.kt
@@ -2,29 +2,35 @@ package ru.suhov.student
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
+import ru.student.assistant.account.AccountFragment
 import ru.student.assistant.auth.ui.AuthFragment
 import ru.student.core.AppConstants.CONTAINER_ID
+import ru.student.core.AppConstants.FRAGMENT_ACCOUNT
+import ru.student.core.AppConstants.FRAGMENT_AUTH
+import ru.student.core.base.ActivityContract
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActivityContract {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(FragmentContainerView(this@MainActivity).apply { id = CONTAINER_ID })
 
-        val container = FragmentContainerView(this@MainActivity)
-        container.id = CONTAINER_ID
-
-        setContentView(container)
-
-        AuthFragment().pushFragment()
+        pushFragmentById(FRAGMENT_AUTH)
     }
 
-    private fun Fragment.pushFragment() {
+    override fun pushFragmentById(id: Int) {
+
+        val fragment = when (id) {
+            FRAGMENT_AUTH -> AuthFragment()
+            FRAGMENT_ACCOUNT -> AccountFragment()
+            else -> AuthFragment()
+        }
+
         this@MainActivity.supportFragmentManager
             .beginTransaction()
             .addToBackStack(null)
-            .replace(CONTAINER_ID, this)
+            .replace(CONTAINER_ID, fragment)
             .commit()
     }
 }
