17bae75248221c08426e38585ee1acecdc66ecce
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-24 15:33:42 +0300

    Minor

--- base/core/src/main/java/ru/student/core/AppConstants.kt
+++ base/core/src/main/java/ru/student/core/AppConstants.kt
@@ -4,16 +4,16 @@ object AppConstants {
 
     const val CONTAINER_ID: Int = 1
 
-    const val FRAGMENT_AUTH: Byte = 0
+    const val FRAGMENT_AUTH: Int = 0
 
-    const val FRAGMENT_EMPTY_SCHEDULE: Byte = 3
-    const val FRAGMENT_SCHEDULE: Byte = 4
+    const val FRAGMENT_EMPTY_SCHEDULE: Int = 3
+    const val FRAGMENT_SCHEDULE: Int = 4
 
-    const val FRAGMENT_CALLS: Byte = 5
-    const val FRAGMENT_CALLS_PREF: Byte = 6
+    const val FRAGMENT_CALLS: Int = 5
+    const val FRAGMENT_CALLS_PREF: Int = 6
 
-    const val FRAGMENT_MAPS: Byte = 7
-    const val FRAGMENT_ACCOUNT: Byte = 8
+    const val FRAGMENT_MAPS: Int = 7
+    const val FRAGMENT_ACCOUNT: Int = 8
 
 
     const val DIALOG_GROUP: Byte = 10
