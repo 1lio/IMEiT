444f67c8a1a127dad39da14eb74977d31549e88e
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-31 15:06:17 +0300

    Minor

--- features/auth/src/main/kotlin/ru/student/assistant/auth/ui/AuthPagerAdapter.kt
+++ features/auth/src/main/kotlin/ru/student/assistant/auth/ui/AuthPagerAdapter.kt
@@ -6,7 +6,11 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class AuthPagerAdapter(fm: FragmentManager, l: Lifecycle) : FragmentStateAdapter(fm, l) {
 
+    private companion object {
+        const val ITEM_COUNT: Int = 2
+    }
+
     override fun createFragment(p: Int) = if (p == 0) SignInFragment() else SignUpFragment()
 
-    override fun getItemCount() = 2
+    override fun getItemCount() = ITEM_COUNT
 }
