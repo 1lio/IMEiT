56b48ad39ab433577e32e6415e3ef3c5fd2bc241
ViacheslavSukhov <svo_efremov@mail.ru>
2020-09-01 15:59:36 +0300

    Minor

---
+++ app/src/main/kotlin/ru/suhov/student/ContainerView.kt
@@ -0,0 +1,52 @@
+package ru.suhov.student
+
+import android.content.Context
+import android.widget.FrameLayout
+import com.google.android.material.bottomappbar.BottomAppBar
+import ru.student.core.AppConstants.CONTAINER_ID
+
+class ContainerView(context: Context) : FrameLayout(context) {
+
+    private val fragmentContainer: FrameLayout = FrameLayout(context)
+    private val appBar: BottomAppBar = BottomAppBar(context)
+
+    init {
+
+        configureContainer()
+
+        configureFragmentContainer()
+
+        configureAppBar()
+
+        merge()
+    }
+
+    private fun configureContainer() {
+        id = CONTAINER_ID
+
+    }
+
+    private fun configureFragmentContainer() {
+        with(fragmentContainer) {
+            id = CONTAINER_FRAGMENT_ID
+        }
+
+    }
+
+    private fun configureAppBar() {
+        with(appBar) {
+            id = APP_BAR_ID
+        }
+    }
+
+
+    private fun merge() {
+        addView(fragmentContainer)
+        addView(appBar)
+    }
+
+    private companion object {
+        const val CONTAINER_FRAGMENT_ID = 100
+        const val APP_BAR_ID = 101
+    }
+}
