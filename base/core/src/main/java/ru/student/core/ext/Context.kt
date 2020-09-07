38391750111a3aba5fc9b35c1eafc5281cd2c9a9
ViacheslavSukhov <svo_efremov@mail.ru>
2020-09-07 16:40:46 +0300

    Add Context extensions (network, etc)

--- base/core/src/main/java/ru/student/core/ext/Context.kt
+++ base/core/src/main/java/ru/student/core/ext/Context.kt
@@ -1,8 +1,14 @@
 package ru.student.core.ext
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 fun Context.showToast(msg: String) {
@@ -15,4 +21,28 @@ fun Fragment.showToast(msg: String) {
 
 fun AppCompatActivity.showToast(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+ fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    } else {
+        val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+        return nwInfo.isConnected
+    }
+}
+
+
+fun View.getDrawable(id:Int): Drawable? {
+    return ContextCompat.getDrawable(this.context, id)
 }
