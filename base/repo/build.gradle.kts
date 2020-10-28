e25af5c07e12df0cd83471c44243104b168adedd
ViacheslavSukhov <svo_efremov@mail.ru>
2020-10-28 10:03:35 +0300

    Add firebase deps

--- base/repo/build.gradle.kts
+++ base/repo/build.gradle.kts
@@ -1,33 +1,14 @@
-android {
-
-    defaultConfig {
-        versionCode = 1
-        versionName = "1.0"
-
-        //
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-}
-
 dependencies {
-
 
     implementation(Config.Libs.Androidx.appCompat)
     implementation(Config.Libs.Androidx.ktx)
 
+    // Firebase repo impl
+    implementation(Config.Libs.Firebase.databaseKtx)
+    implementation(Config.Libs.Firebase.authKtx)
+    implementation(Config.Libs.Firebase.storage)
+
+    // Tests
     testImplementation(Config.Libs.Test.junit)
     androidTestImplementation(Config.Libs.Test.testRunner)
-    androidTestImplementation(Config.Libs.Test.espresso)
 }
