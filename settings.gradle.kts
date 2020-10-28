d0f9e756a89ad3b9cc9ed0fa23cfac4711feb9d4
ViacheslavSukhov <svo_efremov@mail.ru>
2020-10-28 09:42:45 +0300

    Minor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,13 @@ rootProject.name = "IMEiT"
 include(":app")
 
 include(":core")
-project(":core").projectDir =  File(rootDir, "/base/core")
+project(":core").projectDir = File(rootDir, "/base/core")
 
 include(":network")
-project(":network").projectDir =  File(rootDir, "/base/network")
+project(":network").projectDir = File(rootDir, "/base/network")
 
 include(":repo")
-project(":repo").projectDir =  File(rootDir, "/base/repo")
+project(":repo").projectDir = File(rootDir, "/base/repo")
 
 include(":auth")
 project(":auth").projectDir = File(rootDir, "/features/auth")
