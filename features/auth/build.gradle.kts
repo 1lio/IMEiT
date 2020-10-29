17b1a02e86473033ee0b75efbbfbad74eb5beb20
ViacheslavSukhov <svo_efremov@mail.ru>
2020-10-29 16:10:16 +0300

    Add repo/

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -1,30 +1,13 @@
 import Config.Libs.Androidx
-import Config.Libs.Firebase
-import Config.Libs.Kotlin
-
-apply(plugin = "com.google.gms.google-services")
 
 dependencies {
 
-    // Coroutines
-    implementation(Kotlin.coroutinesCore)
-    implementation(Kotlin.coroutinesAndroid)
-    implementation(Kotlin.coroutinesPS)
-
-    // AndroidX
     implementation(Androidx.appCompat)
-    implementation(Androidx.lifecycleExtensions)
     implementation(Androidx.constraint)
     implementation(Androidx.material)
-
-    implementation(Androidx.viewpager)
     implementation(Androidx.vectorAnimation)
-
-    // Firebase
-    implementation(Firebase.authKtx)
-    implementation(Firebase.database)
-    implementation(Firebase.databaseKtx)
-    implementation(Firebase.storage)
+    implementation(Androidx.lifecycleExtensions)
 
     api(project(":core"))
+    api(project(":repo"))
 }
