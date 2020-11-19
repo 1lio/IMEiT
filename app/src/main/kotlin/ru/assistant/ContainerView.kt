5ae281a693c788510ca894ed496beb9b6faeb448
1lio <svoefremov@yandex.ru>
2020-11-19 18:46:41 +0300

    Refactoring -> moved to package "ru.assistant"

--- app/src/main/kotlin/ru/assistant/ContainerView.kt
+++ app/src/main/kotlin/ru/assistant/ContainerView.kt
@@ -1,4 +1,4 @@
-package ru.student.assistant
+package ru.assistant
 
 import android.content.Context
 import android.util.AttributeSet
--- app/src/main/kotlin/ru/assistant/MainActivity.kt
+++ app/src/main/kotlin/ru/assistant/MainActivity.kt
@@ -1,4 +1,4 @@
-package ru.student.assistant
+package ru.assistant
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
