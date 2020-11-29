55ee497bd07feb8d62397efa9eb0eca51acf1d41
1lio <svoefremov@yandex.ru>
2020-11-30 01:01:15 +0300

    Minor

--- base/core/src/main/kotlin/ru/assistant/core/contract/AppNavigation.kt
+++ base/core/src/main/kotlin/ru/assistant/core/contract/AppNavigation.kt
@@ -4,9 +4,10 @@ import ru.assistant.core.AppConstants
 
 interface AppNavigation {
 
-    fun pushFragmentById(id: Byte, container:Int = AppConstants.CONTAINER_ID, now:Boolean = false)
+    fun pushFragmentById(id: Byte, container: Int = AppConstants.CONTAINER_ID, now: Boolean = false)
 
     fun removeFragmentById(id: Byte)
 
     fun showLoader(isShow: Boolean)
+
 }
