d630e8d8589a841542ddc1ae215e66a0c2452ad2
1lio <svoefremov@yandex.ru>
2020-11-30 01:01:24 +0300

    Minor

--- app/src/main/kotlin/ru/assistant/MainActivity.kt
+++ app/src/main/kotlin/ru/assistant/MainActivity.kt
@@ -2,21 +2,92 @@ package ru.assistant
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.assistant.core.AppConstants.CONTAINER_ID
+import androidx.fragment.app.Fragment
+import ru.assistant.auth.ui.AuthFragment
 import ru.assistant.core.AppConstants.FRAGMENT_AUTH
-import ru.assistant.navigation.AppNavigation
+import ru.assistant.core.contract.AppNavigation
 import ru.assistant.ui.ContainerView
 
 // Навигация в приложении происходит через AppNavigation который реализует активити. Стараюсь не
 // тащить посторонние либы в проект
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AppNavigation {
+
+    private lateinit var containerView: ContainerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ContainerView(this@MainActivity).apply { id = CONTAINER_ID })
 
-        if (savedInstanceState == null) AppNavigation(this).pushFragmentById(FRAGMENT_AUTH, now = true)
+        containerView = ContainerView(this@MainActivity)
+
+        setContentView(containerView)
+
+        if (savedInstanceState == null) pushFragmentById(FRAGMENT_AUTH, now = true)
     }
 
+    private val fragmentManager = supportFragmentManager
+
+    override fun pushFragmentById(id: Byte, container: Int, now: Boolean) {
+
+        val fragment = getFragment(id)
+        val tag = fragment::class.java.name
+
+        if (now) {
+            fragmentManager
+                .beginTransaction()
+                .replace(container, fragment, tag)
+                .commitNow()
+        } else {
+            fragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(container, fragment, tag)
+                .commit()
+        }
+
+        when (id) {
+
+            /*  17.toByte() -> {
+                  containerView.isVisibleAppBar = false
+                  containerView.isVisibleFab = false
+              }
+              18.toByte() -> {
+                  containerView.isVisibleAppBar = false
+                  containerView.isVisibleFab = true
+              }
+              19.toByte() -> {
+                  containerView.isVisibleAppBar = true
+                  containerView.isVisibleFab = true
+              }
+              20.toByte() -> {
+                  containerView.isVisibleAppBar = true
+                  containerView.isVisibleFab = false
+              }*/
+
+
+        }
+    }
+
+    override fun removeFragmentById(id: Byte) {
+        fragmentManager.findFragmentByTag(getFragment(id)::class.java.name)
+            ?.let {
+                fragmentManager
+                    .beginTransaction()
+                    .remove(it)
+                    .commit()
+            } ?: return
+    }
+
+    override fun showLoader(isShow: Boolean) {}
+
+
+    // Activity know all main fragments in modules
+    private fun getFragment(id: Byte) = when (id) {
+        FRAGMENT_AUTH -> AuthFragment.newInstance()
+        // FRAGMENT_ACCOUNT -> AccountFragment()
+        // FRAGMENT_MAPS -> MapsFragment()
+        // FRAGMENT_SCHEDULE ->
+        // FRAGMENT_CALLS ->
+
+        else -> Fragment()
+    }
 
 }
